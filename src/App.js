b7787dbc3cee8981efffa1a9ae216a49224699af
Kyle Huang Junyuan <kyle@hjunyuan.com>
2021-01-19 19:00:21 +0800

    Implement automatic retry of API call if it fails

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
+import { useEffect, useState } from "react";
+import getMovies from "./api/getMovies";
 import { Container } from "react-bootstrap";
 
 import Menu from "./components/Menu";
 
 function App() {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    (async function () {
+      const data = await getMovies();
+      setData(data);
+      console.log(data);
+    })();
+  }, []);
+
   return (
     <>
       <Menu />
-      <Container>Hello world</Container>
+      <Container>{JSON.stringify(data)}</Container>
     </>
   );
 }
--- src/api/getMovies.js
+++ src/api/getMovies.js
@@ -2,9 +2,16 @@ import axios from "axios";
 
 const ENDPOINT = "https://sometimes-maybe-flaky-api.gdshive.io";
 
-const getMovies = async () => {
-  const { data } = await axios.get(ENDPOINT);
-  return data;
+const getMovies = async () => axios.get(ENDPOINT);
+
+const getMoviesRetry = async () => {
+  return getMovies()
+    .then((response) => response.data)
+    .catch((err) => {
+      console.log(err);
+      console.log("API unreachable. Retrying now...");
+      return getMoviesRetry();
+    });
 };
 
-export default getMovies;
+export default getMoviesRetry;
