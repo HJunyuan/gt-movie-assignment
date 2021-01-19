0a92a4e8765d323cef7496318e65d143b4f8039c
Kyle Huang Junyuan <kyle@hjunyuan.com>
2021-01-19 20:06:29 +0800

    Implement truncating of synopsis text

--- src/components/Movie/Card.js
+++ src/components/Movie/Card.js
@@ -1,11 +1,13 @@
 import { Link } from "react-router-dom";
 import { Badge, Card } from "react-bootstrap";
 
+const WORD_LIMIT = 18;
+
 function MovieCard(props) {
   const { name, productionYear, genre, synopsisShort, image } = props;
 
   return (
-    <Card style={{ maxWidth: "18rem" }}>
+    <Card style={{ height: "100%", maxWidth: "18rem" }}>
       <Card.Img
         variant="top"
         src={`https://picsum.photos/seed/${image}/300`}
@@ -16,13 +18,22 @@ function MovieCard(props) {
         <Card.Subtitle className="mb-1">
           <Badge variant="info">{genre}</Badge>
         </Card.Subtitle>
-        <Card.Text>{synopsisShort}</Card.Text>
-        <Card.Link as={Link}>More details</Card.Link>
+        <Card.Text>{truncate(synopsisShort, WORD_LIMIT)}</Card.Text>
+        <Card.Link as={Link} to="">
+          More details
+        </Card.Link>
       </Card.Body>
       {/* TODO: Use date-fns */}
       <Card.Footer className="text-muted">{productionYear}</Card.Footer>
     </Card>
   );
+}
+
+function truncate(str, max) {
+  const array = str.trim().split(" ");
+  const ellipsis = array.length > max ? "…" : "";
+
+  return array.slice(0, max).join(" ") + ellipsis;
 }
 
 export default MovieCard;
