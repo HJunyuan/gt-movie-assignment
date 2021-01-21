28a678af47db9eee0861a9f860e837e1ebf9cae1
Kyle Huang Junyuan <kyle@hjunyuan.com>
2021-01-21 17:57:23 +0800

    Implement Movie view

--- src/views/Movie.js
+++ src/views/Movie.js
@@ -1,8 +1,12 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { useParams, Redirect } from "react-router-dom";
-import { Container } from "../components/Container";
+import { Badge, Placeholder } from "rsuite";
+import styled from "styled-components";
+import DOMPurify from "dompurify";
 
 import { MovieContext } from "../contexts/MovieContext";
+
+import { Container } from "../components/Container";
 
 function MovieView() {
   let { year, name } = useParams();
@@ -11,8 +15,19 @@ function MovieView() {
   year = Number(year);
   name = decodeURI(name);
 
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   if (!isReady) {
-    return <Container>Loading...</Container>;
+    return (
+      <Container>
+        <Placeholder.Graph height={400} active />
+        <Placeholder.Paragraph className="my-5" rows={1} rowHeight={30} />
+        <Placeholder.Paragraph rows={10} rowMargin={3} />
+        <Placeholder.Paragraph rows={6} rowMargin={3} />
+      </Container>
+    );
   } else if (
     /* Redirect to 404 when no such movie exist */
     !(
@@ -21,14 +36,42 @@ function MovieView() {
     )
   ) {
     return <Redirect to="/404" />;
-  }
+  } else {
+    /* Display Movie Details */
+    const { genre, synopsis, image } = movies.find(
+      (movie) => movie.productionYear === year && movie.name === name
+    );
 
-  return (
-    <Container>
-      <h1>{decodeURI(name)}</h1>
-      <p>{year}</p>
-    </Container>
-  );
+    return (
+      <Container>
+        <img
+          alt={`${name} (${year})`}
+          style={{
+            width: "100%",
+            height: 400,
+            objectFit: "cover",
+            borderRadius: "0 0 6px 6px",
+          }}
+          src={`https://picsum.photos/seed/${image}/1000`}
+          draggable="false"
+        />
+        <h1 className="my-2">{name}</h1>
+        <p>
+          <b>Production Year</b>: {year}
+        </p>
+        <StyledBadge className="my-2" content={genre} />
+        <br />
+        <div
+          dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(synopsis) }}
+        />
+      </Container>
+    );
+  }
 }
 
 export default MovieView;
+
+const StyledBadge = styled(Badge)`
+  padding: 0.2rem 0.4rem;
+  background: #4caf50;
+`;
