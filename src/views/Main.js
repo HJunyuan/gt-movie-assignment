201cece0079c0bffe33d92fdc4bbae5fc2cec6b7
Kyle Huang Junyuan <kyle@hjunyuan.com>
2021-01-21 15:02:51 +0800

    Simplify Main component to use a single filterParams state

--- src/views/Main.js
+++ src/views/Main.js
@@ -1,106 +1,75 @@
-import { useContext, useEffect, useReducer } from "react";
-import { Container } from "react-bootstrap";
+import { useContext, useEffect, useState } from "react";
+import { Grid, Panel } from "rsuite";
 
 import { MovieContext } from "../contexts/MovieContext";
 
 import { MovieCard, MovieGrid } from "../components/Movie";
 import { GenreFilter, YearFilter } from "../components/Filter";
-
-function initMovieReducer({ movies, genres, years }) {
-  return {
-    /* Dictionaries */
-    allMovies: movies,
-    allGenres: genres,
-    allYears: years,
-
-    /* Variables */
-    filterParams: { genres, years },
-    filtered: movies,
-  };
-}
-
-function filterMovies(movies, filterParams) {
-  const { genres, years } = filterParams;
-  return movies.filter(
-    (movie) =>
-      genres.includes(movie.genre) && years.includes(movie.productionYear)
-  );
-}
-
-function movieReducer(state, action) {
-  const newState = { ...state };
-
-  switch (action.type) {
-    case "filter-years":
-      const { years } = action;
-
-      newState.filterParams.years = [...years];
-      newState.filtered = filterMovies(state.allMovies, newState.filterParams);
-
-      return newState;
-    case "filter-genres":
-      const { genre } = action;
-
-      /* Add all genres */
-      if (genre === "All Genres") {
-        newState.filterParams.genres = state.allGenres;
-      } else {
-        newState.filterParams.genres = [genre];
-      }
-
-      newState.filtered = filterMovies(state.allMovies, newState.filterParams);
-
-      return newState;
-    case "reset":
-      return initMovieReducer(action.payload);
-    default:
-      throw new Error(`Invalid action type: ${action.type}`);
-  }
-}
+import styled from "styled-components";
 
 function Main() {
   const { isReady, movies, genres, years } = useContext(MovieContext);
-  const [movieDictionary, dispatchMovieFilter] = useReducer(
-    movieReducer,
-    { movies, genres, years },
-    initMovieReducer
-  );
+  const [filterParams, setFilterParams] = useState({ genres, years });
 
-  /* Initialise movieDictionary when MovieContext supplies data */
+  /* Initialise filterParams when isReady */
   useEffect(() => {
     if (isReady) {
-      dispatchMovieFilter({
-        type: "reset",
-        payload: { movies, genres, years },
-      });
+      setFilterParams({ genres, years });
     }
-  }, [isReady, movies, genres, years]);
-
-  if (!isReady) {
-    return <Container>Loading...</Container>;
-  }
+  }, [isReady, genres, years]);
 
   return (
-    <Container>
-      <h1 className="mb-4">Movies</h1>
-      <Container className="mb-4 d-flex">
-        <GenreFilter
-          allGenres={genres}
-          onSelect={(genre) => {
-            dispatchMovieFilter({ type: "filter-genres", genre });
-          }}
-        />
-        <YearFilter
-          minYear={years[0]}
-          maxYear={years[years.length - 1]}
-          onChange={(years) => {
-            dispatchMovieFilter({ type: "filter-years", years });
-          }}
-        />
-      </Container>
-      <MovieGrid>{genMovieCards(movieDictionary.filtered)}</MovieGrid>
-    </Container>
+    <Grid>
+      <h1>Movies</h1>
+      <Panel
+        className="my-4"
+        header="Filters"
+        defaultExpanded={false}
+        collapsible
+        bordered
+      >
+        <PanelGrid>
+          <div>
+            <p className="mb-4">
+              <b>By Year</b>
+            </p>
+            <YearFilter
+              allYears={years}
+              onChange={(selected) => {
+                setFilterParams((o) => ({ ...o, years: selected }));
+              }}
+            />
+          </div>
+          <div>
+            <p className="mb-2">
+              <b>By Genres</b>
+            </p>
+            <GenreFilter
+              allGenres={genres}
+              onChange={(selected) => {
+                setFilterParams((o) => ({ ...o, genres: selected }));
+              }}
+            />
+          </div>
+        </PanelGrid>
+      </Panel>
+
+      <MovieGrid>{filteredMovies(movies, filterParams)}</MovieGrid>
+    </Grid>
   );
+}
+
+function filteredMovies(movies, { genres, years }) {
+  if (!movies || !genres || !years) {
+    return mockup();
+  }
+
+  const filtered = movies.filter(
+    (movie) =>
+      (genres.includes(movie.genre) || genres.includes("All Genres")) &&
+      years.includes(movie.productionYear)
+  );
+  return genMovieCards(filtered);
 }
 
 function genMovieCards(movies) {
@@ -109,5 +78,22 @@ function genMovieCards(movies) {
   }
   return movies.map((movie, i) => <MovieCard key={i} {...movie} />);
 }
+
+function mockup() {
+  const count = 6;
+  const mockups = [];
+
+  for (let i = 0; i < count; i++) mockups.push(<MovieCard key={i} mockup />);
+
+  return mockups;
+}
+
+const PanelGrid = styled.div`
+  display: grid;
+  align-items: center;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-column-gap: 1rem;
+  grid-row-gap: 2rem;
+`;
 
 export default Main;
