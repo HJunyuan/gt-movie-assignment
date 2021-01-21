bd7529a459b3890e800ea6031497c430276bd754
Kyle Huang Junyuan <kyle@hjunyuan.com>
2021-01-21 18:08:15 +0800

    Enhance UI of 404 view

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,12 +1,12 @@
 import { Grid } from "rsuite";
 import styled from "styled-components";
 
-function Container({ children }) {
-  return <StyledGrid>{children}</StyledGrid>;
+function Container({ children, ...props }) {
+  return <StyledGrid {...props}>{children}</StyledGrid>;
 }
 
 const StyledGrid = styled(Grid)`
-  padding: 0 2rem;
+  padding: 0 1.3rem;
 `;
 
 export { Container };
--- src/views/404.js
+++ src/views/404.js
@@ -1,15 +1,25 @@
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+
 import { Container } from "../components/Container";
 
 function NotFound() {
   return (
     <Container>
-      <h1>404 Not Found</h1>
-      <p>
-        Let's head back <Link to="/">home</Link>.
-      </p>
+      <Content>
+        <h1>Oops, invalid page.</h1>
+        <p>
+          Let's head back <Link to="/">home</Link>.
+        </p>
+      </Content>
     </Container>
   );
 }
+
+const Content = styled.div`
+  min-height: 80vh;
+  padding-top: 20vh;
+  text-align: center;
+`;
 
 export default NotFound;
