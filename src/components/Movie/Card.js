083ee7df2138ccd8cb77bb37d506b5a3ed659032
Kyle Huang Junyuan <kyle@hjunyuan.com>
2021-01-25 12:02:30 +0800

    WIP: Testing

--- src/components/Movie/Card.js
+++ src/components/Movie/Card.js
@@ -8,85 +8,85 @@ import generateGenreColour from "../../utilities/genreColour";
 const WORD_LIMIT = 18;
 
 function MovieCard(props) {
-  const { name, productionYear, genre, synopsisShort, image, mockup } = props;
-  const history = useHistory();
+	const { name, productionYear, genre, synopsisShort, image, mockup } = props;
+	const history = useHistory();
 
-  function onClick() {
-    history.push(`movie/${productionYear}/${encodeURI(name)}`);
-  }
+	function onClick() {
+		history.push(`movie/${productionYear}/${encodeURI(name)}`);
+	}
 
-  if (mockup) {
-    return (
-      <Card shaded bodyFill style={{ width: "100%" }}>
-        <Placeholder.Graph height={300} active />
-        <Card>
-          <Placeholder.Paragraph rows={1} rowHeight={25} />
-          <Placeholder.Paragraph rows={4} rowMargin={3} />
-        </Card>
-      </Card>
-    );
-  }
+	if (mockup) {
+		return (
+			<Card shaded bodyFill style={{ width: "100%" }}>
+				<Placeholder.Graph height={300} active />
+				<Card>
+					<Placeholder.Paragraph rows={1} rowHeight={25} />
+					<Placeholder.Paragraph rows={4} rowMargin={3} />
+				</Card>
+			</Card>
+		);
+	}
 
-  return (
-    <Card shaded bodyFill onClick={onClick}>
-      <img
+	return (
+		<Card shaded bodyFill onClick={onClick} data-testid="card">
+			{/* <img
         alt={`${name} (${productionYear})`}
         style={{ width: "100%", height: 300, objectFit: "cover" }}
         src={`https://picsum.photos/seed/${image}/480`}
         draggable="false"
-      />
-      <Card>
-        <CardTitle>{`${name} (${productionYear})`}</CardTitle>
-        <CardBadge className="mb-2" content={genre} />
-        <CardDescription>{truncate(synopsisShort, WORD_LIMIT)}</CardDescription>
-        <CardFooter>
-          <p>{formatDistance(new Date(productionYear, 0), new Date())} ago</p>
-          <IconButton icon={<Icon icon="arrow-right" />} placement="right" />
-        </CardFooter>
-      </Card>
-    </Card>
-  );
+      /> */}
+			<Card>
+				<CardTitle>{`${name} (${productionYear})`}</CardTitle>
+				<CardBadge className="mb-2" content={genre} />
+				<CardDescription>{truncate(synopsisShort, WORD_LIMIT)}</CardDescription>
+				<CardFooter>
+					<p>{formatDistance(new Date(productionYear, 0), new Date())} ago</p>
+					<IconButton icon={<Icon icon="arrow-right" />} placement="right" />
+				</CardFooter>
+			</Card>
+		</Card>
+	);
 }
 
 function truncate(str, max) {
-  const array = str.trim().split(" ");
-  const ellipsis = array.length > max ? "…" : "";
+	const array = str.trim().split(" ");
+	const ellipsis = array.length > max ? "…" : "";
 
-  return array.slice(0, max).join(" ") + ellipsis;
+	return array.slice(0, max).join(" ") + ellipsis;
 }
 
 const Card = styled(Panel)`
-  width: 100%;
-  height: 100%;
-  max-width: 360px;
+	width: 100%;
+	height: 100%;
+	max-width: 360px;
 
-  &:hover {
-    transform: scale(1.02);
-    cursor: pointer;
-  }
-  transition: transform 0.6s cubic-bezier(0.25, 1, 0.5, 1);
+	&:hover {
+		transform: scale(1.02);
+		cursor: pointer;
+	}
+	transition: transform 0.6s cubic-bezier(0.25, 1, 0.5, 1);
 `;
 
 const CardTitle = styled.h2`
-  font-size: 1.4rem;
-  line-height: 1.7rem;
+	font-size: 1.4rem;
+	line-height: 1.7rem;
 
-  margin-bottom: 1rem;
+	margin-bottom: 1rem;
 `;
 
 const CardBadge = styled(Badge)`
-  padding: 0.2rem 0.4rem;
-  background: ${({ content }) => generateGenreColour(content)};
+	padding: 0.2rem 0.4rem;
+	background: ${({ content }) => generateGenreColour(content)};
 `;
 
 const CardDescription = styled.p`
-  margin-bottom: 1rem;
+	margin-bottom: 1rem;
 `;
 
 const CardFooter = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
 `;
 
 export default MovieCard;
--- src/contexts/MovieContext.js
+++ src/contexts/MovieContext.js
@@ -5,26 +5,26 @@ import getMovies from "../api/getMovies";
 export const MovieContext = createContext();
 
 export const MovieProvider = ({ children }) => {
-  const [data, setData] = useState({ isReady: false });
+	const [data, setData] = useState({ isReady: false });
 
-  useEffect(() => {
-    (async function () {
-      const movies = await getMovies();
+	useEffect(() => {
+		(async function () {
+			const movies = await getMovies();
 
-      const uniqueYears = new Set(movies.map((movie) => movie.productionYear));
-      const sortedYears = Array.from(uniqueYears).sort();
+			const uniqueYears = new Set(movies.map((movie) => movie.productionYear));
+			const sortedYears = Array.from(uniqueYears).sort();
 
-      const uniqueGenres = new Set(movies.map((movie) => movie.genre));
-      const sortedGenres = Array.from(uniqueGenres).sort();
+			const uniqueGenres = new Set(movies.map((movie) => movie.genre));
+			const sortedGenres = Array.from(uniqueGenres).sort();
 
-      setData({
-        isReady: true,
-        movies,
-        years: sortedYears,
-        genres: sortedGenres,
-      });
-    })();
-  }, []);
+			setData({
+				isReady: true,
+				movies,
+				years: sortedYears,
+				genres: sortedGenres,
+			});
+		})();
+	}, []);
 
-  return <MovieContext.Provider value={data}>{children}</MovieContext.Provider>;
+	return <MovieContext.Provider value={data}>{children}</MovieContext.Provider>;
 };
